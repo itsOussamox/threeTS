b901807885561dca13dae0085520dc937282cd18
itsOussamox <oussaroublard@gmail.com>
2023-12-30 20:03:31 +0100

    :gear: Added Physics to the scene and a 3D Text with some animation

--- src/app/components/AniBox.tsx
+++ src/app/components/AniBox.tsx
@@ -1,12 +1,14 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import React, { Ref, useRef, useState } from 'react'
+import { Canvas, ThreeEvent, useFrame, useThree } from '@react-three/fiber'
 import { animated, useSpring } from '@react-spring/three'
+import {GradientTexture} from '@react-three/drei'
+import { BoxProps, SphereProps, Triplet, useBox, useSphere } from '@react-three/cannon'
 const bigBoxSize = [3, 0.5, 4];
-const smallBoxSize = [1, 0.5, 6];
+const smallBoxSize = [1, 0.5, 6,3,3,3];
 export function BigBox(props: JSX.IntrinsicElements['mesh']) {
-    const meshRef = useRef<THREE.Mesh>(null!)
+    const [meshRef, api]  = useBox(() => ({ mass: 1, position: props.position as Triplet,
+        args: bigBoxSize as Triplet, rotation: props.rotation as Triplet }))
     const [isHovered, setIsHovered] = useState(false)
-    const [boxRotation, setBoxRotation] = useState(0)
     const aniprops = useSpring({
         scale : isHovered ? 1.5 : 1,
         config(key) {
@@ -16,26 +18,23 @@ export function BigBox(props: JSX.IntrinsicElements['mesh']) {
             }
         },
     })
-    // useFrame((state, delta) => {
-    //     meshRef.current.rotation.x = meshRef.current.rotation.y += boxRotation;
-    // })
-
+    console.log(api)
     return (
         <animated.mesh scale={aniprops.scale} 
         onPointerOver={() => setIsHovered(true)} 
-        onPointerOut={() => {setIsHovered(false); setBoxRotation(0)}}
-        ref={meshRef} {...props}>
-            <boxGeometry args={bigBoxSize as [number,number,number]} />
-            <meshStandardMaterial color='#00ffff' />
+        onPointerOut={() => {setIsHovered(false)}}
+        ref={meshRef as Ref<THREE.Mesh>} {...props}>
+            <boxGeometry args={bigBoxSize as []} />
+            <meshPhysicalMaterial color={'#44476F'}/>
         </animated.mesh>
     )
 }
 
 
 export function SmallBox(props: JSX.IntrinsicElements['mesh']) {
-    const meshRef = useRef<THREE.Mesh>(null!)
+    const [meshRef, api] = useBox(() => ({ mass: 1, position: props.position as any,
+        args: smallBoxSize as any, rotation: props.rotation as any}))
     const [isHovered, setIsHovered] = useState(false)
-    const [boxRotation, setBoxRotation] = useState(0)
     const aniprops = useSpring({
         scale : isHovered ? 1.5 : 1,
         config(key) {
@@ -45,26 +44,22 @@ export function SmallBox(props: JSX.IntrinsicElements['mesh']) {
             }
         },
     })
-    // useFrame((state, delta) => {
-    //     meshRef.current.rotation.x = meshRef.current.rotation.y += boxRotation;
-    // })
-
     return (
         <animated.mesh scale={aniprops.scale} 
         onPointerOver={() => setIsHovered(true)} 
-        onPointerOut={() => {setIsHovered(false); setBoxRotation(0)}}
-        ref={meshRef} {...props}>
-            <boxGeometry args={smallBoxSize as [number,number,number]} />
-            <meshStandardMaterial color='#9A9BD3' />
+        onPointerOut={() => {setIsHovered(false)}}
+        ref={meshRef as Ref<THREE.Mesh>} {...props}>
+            <boxGeometry args={smallBoxSize as []} />
+            <meshPhysicalMaterial color='#9A9BD3' />
         </animated.mesh>
     )
 }
 
 
 export function Ball(props: JSX.IntrinsicElements['mesh']) {
-    const meshRef = useRef<THREE.Mesh>(null!)
+    const [meshRef, api] = useSphere(() => ({ mass: 1,
+        position: props.position as Triplet}))
     const [isHovered, setIsHovered] = useState(false)
-    const [boxRotation, setBoxRotation] = useState(0)
     const aniprops = useSpring({
         scale : isHovered ? 1.5 : 1,
         config(key) {
@@ -74,11 +69,17 @@ export function Ball(props: JSX.IntrinsicElements['mesh']) {
             }
         },
     })
+
+    const applyBallEffect = (e: ThreeEvent<MouseEvent>) => {
+        const [x,y,z] = e.intersections[0].point.toArray();
+        api.
+        api.applyForce([0, 1000, 0], [x,y,z])
+    }
+
     return (
-        <animated.mesh ref={meshRef} {...props}
-            scale={aniprops.scale} 
-            onPointerOver={() => setIsHovered(true)} 
-            onPointerOut={() => {setIsHovered(false); setBoxRotation(0)}} >
+        <animated.mesh ref={meshRef as Ref<THREE.Mesh>} {...props}
+            scale={aniprops.scale}
+            onClick={(e) => {applyBallEffect(e)}}>
             <sphereGeometry args={[1,64,64]}/>
             <meshPhysicalMaterial color={"#44476F"} />
         </animated.mesh>
--- src/app/components/Simulation.tsx
+++ src/app/components/Simulation.tsx
@@ -1,36 +1,71 @@
 'use client';
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useRef, useState, Suspense, useEffect} from "react";
+import { Canvas, useFrame, useThree, extend, Object3DNode, useLoader } from "@react-three/fiber";
+import { useRef, useState, Suspense, useEffect, Ref} from "react";
 import { Environment, GradientTexture, OrbitControls, Plane, useHelper } from "@react-three/drei";
 import { animated, useSpring } from "@react-spring/three";
-import { AxesHelper, DirectionalLightHelper } from "three";
+import { AxesHelper, DirectionalLightHelper, DoubleSide, Euler, SpotLightHelper } from "three";
 import { Ball, BigBox, SmallBox } from "./AniBox";
+import { TextGeometry, FontLoader, Font } from "three/examples/jsm/Addons.js";
+import { Physics, usePlane } from "@react-three/cannon";
+extend({ TextGeometry });
+
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    textGeometry: Object3DNode<TextGeometry, typeof TextGeometry>;
+  }
+}
+extend({ TextGeometry });
+
+function Ground(props: any) {
+   const [ref] : any = usePlane(() => ({rotation: [-Math.PI / 2, 0, 0], ...props }))
+   return (
+     <mesh ref={ref} receiveShadow>
+       <planeGeometry args={[100,100]}/>
+       <meshPhysicalMaterial color={'#44476F'} side={DoubleSide}/>
+     </mesh>
+   )
+}
 
 
 
-
-function Ground() {
-    return (
-      <>
-        <Plane args={[100, 100]} rotation={[-Math.PI/2, 0, 0]} position={[0, 0, 0]} >
-          <meshPhysicalMaterial color="#050A27" />
-        </Plane>
-      </>
-    )
+function TextRender(props: JSX.IntrinsicElements['mesh']) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const myFont = useLoader(FontLoader, '/fonts/Poppins.json');
+  const [onClick, setOnClick] = useState(false);
+  const aniprops = useSpring({
+    rotation: onClick ? [3*Math.PI / 2,0,(Math.PI*2)*2] as any : [3*Math.PI / 2,0,0] as any,
+    config(key) {
+      switch (key) {
+        case 'rotation': return { mass: 1, tension: 1000, friction: 300, precision: 0.0001, velocity: 0 };
+        default: return { mass: 1, tension: 500, friction: 50, precision: 0.0001, velocity: 0 };
+      }
+    },
+  })
+  return (
+    <animated.mesh rotation={aniprops.rotation}
+      onClick={() => {if (onClick) setOnClick(false); else setOnClick(true); console.log('clicked')}}
+      ref={meshRef} {...props}>
+      <textGeometry args={['PONG', {font: myFont, size:1.5, height: 0.1, curveSegments: 64}]}/>
+      <meshLambertMaterial attach='material' color={'white'}/>
+    </animated.mesh>
+  )
 }
 
 function Scene() {
-  const lightRef = useRef<THREE.DirectionalLight>(null!);
-  useHelper(lightRef, DirectionalLightHelper, 1, 'white')
+  const spotlightRef = useRef<THREE.SpotLight>(null!);
+  useHelper(spotlightRef, SpotLightHelper, 'white')
   return (
     <>
-       <ambientLight intensity={0.2}/>
-        <directionalLight ref={lightRef}  position={[10, 10, 10]} intensity={1} castShadow/>
+       <ambientLight intensity={1}/>
+        <spotLight ref={spotlightRef} penumbra={0.1} position={[15, 8, -10]} intensity={10000} color={'#9A9BD3'}  castShadow/>
+        {/* <directionalLight ref={lightRef} position={[15, 8, -10]} intensity={100} color={'#9A9BD3'} castShadow/> */}
+        {/* <StarsSet/> */}
         <Ground />
-        <BigBox  position={[-5, 0.5, -9.5]} rotation={[0,3*Math.PI/3,0]} castShadow/>
-        <SmallBox position={[5, 0.5, -10]} rotation={[0, Math.PI+0.5,0]}/>
+        <BigBox  position={[-5, 10, -9.5]}  castShadow/>
+        <TextRender position={[-2,0,-6]} scale={1} castShadow />
+        <SmallBox position={[5, 0.1, -10]} rotation={[0, Math.PI+0.5,0]} castShadow/>
         <Ball position={[5,1,0]} castShadow/>
-        <Environment preset="dawn"/>
+        <Environment preset="night"/>
         <OrbitControls />
         <axesHelper />
     </>
@@ -40,8 +75,11 @@ function Scene() {
 
 export default function Simulation() {
     return (
-        <Canvas camera={{position: [0,2,5]}} shadows>
+        <Canvas camera={{position: [0,5,5]}} shadows>
+          <color attach='background' args={['#44476F']} />
+          <Physics gravity={[0, -9.8, 0]} >
            <Scene />
+          </Physics>
         </Canvas>
     )
 
